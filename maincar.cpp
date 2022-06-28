52df51d7f78fc334ead680286aacef226fabb933
hananhusen <108353959+hananhusen@users.noreply.github.com>
2022-06-28 21:03:50 +0300

    Update maincar.cpp

--- maincar.cpp
+++ maincar.cpp
@@ -27,3 +27,64 @@ void init() {
     gluOrtho2D(-100, 100, -100, 100);
 
 }
+
+//
+
+
+
+
+
+
+
+
+void timer(int) {
+    
+    glutPostRedisplay();
+    glutTimerFunc(1000/60, timer, 0);
+    cloud1x += 0.30;
+    cloud2x += 0.35;
+    
+    //if cloud is outside the viewport
+    if (cloud1x > 120) {
+        cloud1x = -120;
+    }
+
+    if (cloud2x > 190) {
+        cloud2x = -100;
+    }
+    
+    angle += 5;
+    carAndWheelX +=1;
+    //if car is outside the viewport
+    //change the color to any color from 0 to 254 + 20(to avoid darker colors)
+    if (carAndWheelX > 250) {
+        red = rand() % 255 + 20;
+        green = rand() % 255 + 20;
+        blue = rand() % 255 + 20;
+        
+        redf = (float)red/255;
+        greenf = (float)green/255;
+        bluef = (float)blue/255;
+        carAndWheelX = -50;
+    }
+        
+}
+
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitWindowSize(1200, 800);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowPosition(200, 0);
+
+    glutCreateWindow("18101516 - 18101737");
+
+    init();
+
+    glutDisplayFunc(display);
+    glutTimerFunc(0, timer, 0);
+
+    glutMainLoop();
+
+    return 0;
+}
